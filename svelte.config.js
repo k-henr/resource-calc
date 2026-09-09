ea0194f99283688400d5389231dacee2528916af
k-henr <contact@khenr.se>
2026-09-09 11:36:11 +0200

    GHP stuff

---
+++ svelte.config.js
@@ -0,0 +1,21 @@
+import { mdsvex } from 'mdsvex';
+import adapter from '@sveltejs/adapter-static';
+
+const dev = process.env.NODE_ENV === 'development';
+
+/** @type {import('@sveltejs/kit').Config} */
+
+export default {
+	kit: {
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: null
+		}),
+		paths: {
+			base: dev ? '' : '/YOUR_REPO_NAME'
+		}
+	},
+	preprocess: [mdsvex()],
+	extensions: ['.svelte', '.svx']
+};
