c76b195df196326d1412c1042957a5282824149e
k-henr <contact@khenr.se>
2026-09-09 12:29:51 +0200

    test

--- svelte.config.js
+++ svelte.config.js
@@ -18,8 +18,6 @@ import adapter from '@sveltejs/adapter-static';
 
 import { mdsvex } from 'mdsvex';
 
-const dev = process.env.NODE_ENV === 'development';
-
 /** @type {import('@sveltejs/kit').Config} */
 export default {
 	kit: { adapter: adapter(), paths: { base: '' } },
