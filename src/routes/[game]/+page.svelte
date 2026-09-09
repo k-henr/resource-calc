f7f268198f6ef4c2970b891402f5994c3b32bcbf
k-henr <contact@khenr.se>
2026-09-09 12:27:40 +0200

    Last hosting fix?

--- src/routes/[game]/+page.svelte
+++ src/routes/[game]/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { page } from '$app/state';
-	import { onMount } from 'svelte';
+	import {onMount} from 'svelte';
 
 	onMount(async () => {
 		const game = page.params.game;
--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,28 @@
-import { mdsvex } from 'mdsvex';
+// import adapter from '@sveltejs/adapter-auto';
+// /** @type {import('@sveltejs/kit').Config} */
+// const config = {
+// 	compilerOptions: {
+// 		// Force runes mode for the project, except for libraries. Can be removed in svelte 6.
+// 		runes: ({ filename }) => (filename.split(/[/\\]/).includes('node_modules') ? undefined : true)
+// 	},
+// 	kit: {
+// 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
+// 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+// 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
+// 		adapter: adapter()
+// 	}
+// };
+// export default config;
+// The above was replaced by the below to host on GHP, undo when no longer using GHP
 import adapter from '@sveltejs/adapter-static';
+
+import { mdsvex } from 'mdsvex';
 
 const dev = process.env.NODE_ENV === 'development';
 
 /** @type {import('@sveltejs/kit').Config} */
 export default {
-	kit: {
-		adapter: adapter(),
-		paths: { base: dev ? '' : '/YOUR_REPO_NAME' }
-	},
+	kit: { adapter: adapter(), paths: { base: '' } },
 	preprocess: [mdsvex()],
 	extensions: ['.svelte', '.svx']
 };
